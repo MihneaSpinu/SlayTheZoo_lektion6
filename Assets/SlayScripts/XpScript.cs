674a3b66df02327efefba00f378a74c4c334928c
Samuel <samuel@families.dk>
2023-04-17 13:57:01 +0200

    Start of xp + need Jesper to help

---
+++ Assets/SlayScripts/XpScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XpScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public class Health
+
+    public event EventHandler  
+
+    public LevelSystem levelSystem;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Health <= 0)
+    }
+}
